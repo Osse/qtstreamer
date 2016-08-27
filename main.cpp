52a9e9a9ac8dd43e2f186b56d456cbedbc5a2b19
Øystein Walle <oystwa@gmail.com>
2016-08-27 23:09:48 +0200

    Be able to compile with a static Qt

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "dialog.h"
 #include <QApplication>
 
+#ifdef QT_STATIC
+    #include <QtCore/QtPlugin>
+    Q_IMPORT_PLUGIN (QWindowsIntegrationPlugin)
+#endif
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
